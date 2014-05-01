8e32d1a2bf18fd2fbed86bd5d283edc96a85b0da
Hack Reactor Students <sjsnider@gmail.com>
2014-04-30 17:21:20 -0700

    queue works

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,19 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    //this.set('songCollection', new Songs());
+    this.on('add', function(song){
+      this.add(song);
+      debugger;
+      if(this.models.length===1){
+        this.playFirst(song);
+      }
+    }, this);
+  },
 
+  playFirst: function(song){
+    debugger;
+    this.models[0].play();
+    //this.get('songCollection').add(song);
+  }
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -16,6 +16,15 @@ var AppModel = Backbone.Model.extend({
     params.library.on('play', function(song){
       this.set('currentSong', song);
     }, this);
+
+    params.library.on('enqueue', function(song){
+      // console.log(this.get('songQueue'));
+      //debugger;
+      this.get('songQueue').add(song);
+    }, this);
+
   }
+
+  // this.get('librray')
 
 });
